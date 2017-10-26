870a8f73366be691838e5398d85e42bf03dc21fe
RuurddeRonde <31432750+RuurddeRonde@users.noreply.github.com>
2017-10-26 09:38:57 +0200

    added kinect program used for testing kinect

---
+++ kinect oplevering/kinect oplevering/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Kinect;
+
+namespace kinect_oplevering
+{
+    public partial class Form1 : Form
+    {
+        private KinectSensor kinect;
+        //Depth Frame
+        //private WriteableBitmap bitmap;
+        private ushort[] depthFrameData = null;
+        private byte[] depthPixels = null;
+        public Form1()
+        {
+            InitializeComponent();
+            kinect = KinectSensor.GetDefault();
+            kinect.Open();
+
+
+        }
+
+    private void buttonConnect_Click(object sender, EventArgs e)
+    {
+
+    }
+}
+}
